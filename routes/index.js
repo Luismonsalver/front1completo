f683247f9d5c6949bbe58bd6e085c772d60d66c0
Luismonsalver <luis.monri12@gmail.com>
2023-06-17 01:43:05 -0400

    Pequeño cambio

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Diario UVM Sport' });
+  res.render('index',);
 });
 module.exports = router;
